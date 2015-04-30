e4c83d60805cf177604feb58bed2c39827d0492f
HT154 <ht154@ht154.com>
2015-04-30 00:20:27 -0700

    Initial Commit

--- autoloc/AppDelegate.m
+++ autoloc/AppDelegate.m
@@ -8,18 +8,50 @@
 
 #import "AppDelegate.h"
 
+#import <IOKit/ps/IOPowerSources.h>
+#import <notify.h>
+
+#define HOME_POWER_APADTER_KEY (@"")
+
 @interface AppDelegate ()
 
 @end
 
-@implementation AppDelegate
+@implementation AppDelegate {
+    int token;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    [self powerChanged];
+    
+    notify_register_dispatch(kIOPSNotifyPowerSource, &token, dispatch_get_main_queue(), ^(int token) {
+        [self powerChanged];
+    });
+}
+
+-(void)powerChanged {
+    NSString *loc = @"Automatic";
+    
+    CFDictionaryRef pSource = IOPSCopyExternalPowerAdapterDetails();
+    
+    if (pSource) {
+        const void *psValue;
+        if ((psValue = CFDictionaryGetValue(pSource, @kIOPSPowerAdapterSerialNumberKey))) {
+            if ([(__bridge NSNumber *)psValue isEqualToNumber:@0x0035f890]) {
+                loc = @"Home";
+            }
+        }
+    }
+    
+    NSTask *task = [[NSTask alloc] init];
+    task.launchPath = @"/usr/sbin/scselect";
+    task.arguments = @[loc];
+    
+    [task launch];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+    notify_cancel(token);
 }
 
 @end
